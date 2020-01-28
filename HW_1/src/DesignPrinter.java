4822d68e93d749ed8564baf532e7ffcafbeaec91
Morgan <morganw2001@gmail.com>
2020-01-28 17:35:31 -0500

    number 1

--- HW_1/src/DesignPrinter.java
+++ HW_1/src/DesignPrinter.java
@@ -1,2 +1,26 @@
 public class DesignPrinter {
+    public static void main (String []args){
+        printDesign(5);
+
+
+    }
+
+    //should be able to go up to n = 5 only
+    public static void printDesign(int n){
+
+        for (int i =0; i<n; i++){
+
+            for (int j=n-i;j>1;j--){
+
+                
+            }
+        }
+
+
+
+
+
+    }
+
+
 }
