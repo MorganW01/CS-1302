24757872d36c42d56618174d9989c8f392206697
Morgan <morganw2001@gmail.com>
2020-01-31 11:14:18 -0500

    LAB 3

---
+++ Lab_3/src/Product.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Product {
+    public static void main (String []args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter at least 3 numbers in a space separated list: ");
+        String input = scanner.nextLine();
+        int product=0;
+
+
+
+
+
+
+    }
+
+
+
+
+}
