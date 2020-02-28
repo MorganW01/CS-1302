24340cf2d8768966321b5e73d43fe517096ea11d
Morgan <morganw2001@gmail.com>
2020-02-28 08:52:44 -0500

    begin q1

--- Lab_7/src/Lab_7.java
+++ Lab_7/src/Lab_7.java
@@ -1,8 +1,29 @@
+import java.util.Set;
+
 public class Lab_7 {
+
     public static void main(String []args){
 
+
+    }
+
+    public static Set <Integer> symmetricSetDifference (Set <Integer> set1,Set set2){
+
+        // returns a set of numbers that do not appear in both sets.
+        Set <Integer> finalSet=null;
+
+        //nested for loop maybe? goes through both to check if there are any similar
+        for (int i=0; i<set2.size(); i++){
+            if (set1.contains){
+
+
+            }
+
+
+        }
         
 
+        return finalSet;
 
 
 
