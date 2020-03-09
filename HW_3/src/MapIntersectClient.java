3e35b054184682ad84861b701cba2eebf84d2693
Morgan <morganw2001@gmail.com>
2020-03-08 23:39:57 -0400

    test

---
+++ HW_3/src/MapIntersectClient.java
@@ -0,0 +1,41 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapIntersectClient {
+    public static void main (String [] args){
+
+
+    }
+
+
+    /*returns a new map containing only
+    the key/value pairs that exist in both of the parameter maps.*/
+
+
+
+
+    //Question 1
+
+    //containsValue and containsKey
+    public static Map <String, Integer> mapIntersect (Map <String,Integer> map1, Map <String,Integer> map2){
+        Map <String, Integer> intersect= new HashMap<>();
+
+        
+
+
+
+
+
+
+        return intersect;
+
+
+
+
+
+    }
+
+
+
+}
+
