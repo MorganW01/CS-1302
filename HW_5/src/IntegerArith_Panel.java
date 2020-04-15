8e50b75bca26ae7e14541148aebb356b403a03fa
Morgan <morganw2001@gmail.com>
2020-04-14 18:57:51 -0700

    code

--- HW_5/src/IntegerArith_Panel.java
+++ HW_5/src/IntegerArith_Panel.java
@@ -2,9 +2,28 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-public class IntegerArith_Panel {
+public class IntegerArith_Panel extends JPanel {
     private JTextField number1;
     private JTextField number2;
+    private JButton display;
+
+    private JLabel number2Label, number1Label, displayLabel;
+
+    public IntegerArith_Panel(){
+
+
+        number1Label = new JLabel("first number: ");
+        number2Label= new JLabel("second number: ");
+        displayLabel = new JLabel("display");
+
+        add(number1Label);
+        add(number2Label);
+        add(displayLabel);
+
+        setPreferredSize();
+
+
+    }
 
 
 
