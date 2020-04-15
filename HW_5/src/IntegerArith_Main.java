f03d943ca170549a5c91f63f7debb2cebd9b1671
Morgan <morganw2001@gmail.com>
2020-04-14 19:49:49 -0700

    done with question 4

--- HW_5/src/IntegerArith_Main.java
+++ HW_5/src/IntegerArith_Main.java
@@ -1,5 +1,13 @@
+import javax.swing.*;
+
 public class IntegerArith_Main {
-
-
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Question 4");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        IntegerArith_Panel panel = new IntegerArith_Panel();
+        frame.getContentPane().add(panel);
+        frame.pack();
+        frame.setVisible(true);
+    }
 
 }
--- HW_5/src/IntegerArith_Panel.java
+++ HW_5/src/IntegerArith_Panel.java
@@ -3,30 +3,47 @@ import java.awt.event.*;
 import javax.swing.*;
 
 public class IntegerArith_Panel extends JPanel {
-    private JTextField number1;
-    private JTextField number2;
+    private JTextField number1, number2;
     private JButton display;
+    private JLabel sumText, productText, label1, label2, sumOutput, productOutput;
 
-    private JLabel number2Label, number1Label, displayLabel;
+    public IntegerArith_Panel() {
 
-    public IntegerArith_Panel(){
+        label1 = new JLabel("Enter first number: ");
+        label2 = new JLabel("Enter second number: ");
+        display = new JButton("Display");
+        sumText = new JLabel("Sum = ");
+        productText = new JLabel("Product = ");
+        sumOutput = new JLabel("---");
+        productOutput = new JLabel("---");
+        number1 = new JTextField(5);
+        number1.addActionListener(new ButtonListener());
+        number2 = new JTextField(5);
+        number2.addActionListener(new ButtonListener());
+        add(label1);
+        add(number1);
+        add(label2);
+        add(number2);
+        add(display);
+        add(sumText);
+        add(sumOutput);
+        add(productText);
+        add(productOutput);
 
-
-        number1Label = new JLabel("first number: ");
-        number2Label= new JLabel("second number: ");
-        displayLabel = new JLabel("display");
-
-        add(number1Label);
-        add(number2Label);
-        add(displayLabel);
-
-        setPreferredSize();
-
-
+        setPreferredSize(new Dimension(300, 40));
+        setBackground(Color.orange);
     }
+    private class ButtonListener implements ActionListener {
+        public void actionPerformed(ActionEvent event) {
+            int num1, num2;
+            String text1 = number1.getText();
+            String text2 = number2.getText();
+            num1 = Integer.parseInt(text1);
+            num2 = Integer.parseInt(text2);
+            sumOutput.setText(Integer.toString(num1 + num2));
+            productOutput.setText(Integer.toString(num1 * num2));
 
 
-
-
-
+        }
+    }
 }
